bf87532f508d1ef56d5373c9543d781d4bda4beb
Hannah Smith <Hannah.Smith@hyland.com>
2018-12-03 17:54:11 -0500

    Day 2 Part B successful

--- src/02/day2_test.go
+++ src/02/day2_test.go
@@ -49,3 +49,37 @@ func CountAllLetters(s string) map[rune]int {
 	}
 	return letterMap
 }
+
+func TestPartB(t *testing.T) {
+	lines, _ := helper.ReadLines("input")
+
+	for i1, line1 := range lines {
+		for i2, line2 := range lines {
+			if i2 <= i1 {
+				continue
+			}
+
+			differentLetters := 0
+			var diffList []int
+
+			for j := range line1 {
+				if line1[j] != line2[j] {
+					differentLetters++
+					diffList = append(diffList, j)
+				}
+			}
+
+			if differentLetters == 1 {
+				fmt.Print("Part B: ")
+				for j, let := range line1 {
+					if j != diffList[0] {
+						fmt.Printf("%c", let)
+					}
+				}
+
+			}
+		}
+	}
+
+	fmt.Println()
+}
