63a31ab0756c4335ffa169e60ddd0a6e3eaa7bbc
Hannah Smith <Hannah.Smith@hyland.com>
2018-12-03 16:17:41 -0500

    Day1, Part B success

--- src/01/day1_test.go
+++ src/01/day1_test.go
@@ -17,7 +17,28 @@ func TestPartA(t *testing.T) {
 		NewFrequency(int(i))
 	}
 
-	fmt.Println(Frequency)
+	fmt.Printf("Part A: %v \n", Frequency)
+}
+
+func TestPartB(t *testing.T) {
+	Frequency = 0
+	lines, _ := readLines("input")
+
+	var frequencies []int
+	frequencies = append(frequencies, Frequency)
+
+	for true {
+		for _, value := range lines {
+			i, _ := strconv.ParseInt(value, 10, 32)
+			NewFrequency(int(i))
+			if IsUnique(Frequency, frequencies) {
+				frequencies = append(frequencies, Frequency)
+			} else {
+				fmt.Printf("Part B: %v \n", Frequency)
+				return
+			}
+		}
+	}
 }
 
 func readLines(path string) ([]string, error) {
@@ -34,4 +55,14 @@ func readLines(path string) ([]string, error) {
 	}
 
 	return lines, scanner.Err()
+}
+
+func IsUnique(value int, list []int) bool {
+	for _, item := range list {
+		if item == value {
+			return false
+		}
+	}
+
+	return true
 }
