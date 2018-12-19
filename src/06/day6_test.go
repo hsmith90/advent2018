d92ff174b48028fa779f4a200d5685094a47dae1
Hannah Smith <Hannah.Smith@hyland.com>
2018-12-19 16:49:34 -0500

    Day 6

--- src/06/day6_test.go
+++ src/06/day6_test.go
@@ -9,115 +9,166 @@ import (
 	"testing"
 )
 
+type Point struct {
+	closestLocation        int
+	isBaseLocation         bool
+	distanceToAllLocations int
+}
+
 func TestPartA(t *testing.T) {
-	lines, _ := helper.ReadLines("test")
-	var locations []Location
+	locations, lCount := GetLocations("input")
+	maxDistance := 10000
+
+	for y, line := range locations {
+		for x, p := range line {
+			p.FindClosestLocation(x, y, locations)
+
+		}
+	}
+
+	largestArea := 0
+
+	for i := 0; i < lCount; i++ {
+		area, isInfinite := GetArea(i, locations)
+
+		if !isInfinite {
+			if area > largestArea {
+				largestArea = area
+			}
+		}
+	}
+
+	areaClosestToAll := 0
+
+	for _, line := range locations {
+		for _, p := range line {
+			if p.distanceToAllLocations < maxDistance {
+				areaClosestToAll++
+			}
+		}
+	}
+
+	fmt.Printf("Part A: %v\n", largestArea)
+	fmt.Printf("Part B: %v\n", areaClosestToAll)
+
+}
+
+func GetLocations(file string) ([][]*Point, int) {
+	lines, _ := helper.ReadLines(file)
+	xMax, yMax, xMin, yMin := 0, 0, math.MaxInt32, math.MaxInt32
+
+	for _, line := range lines {
+		coords := strings.Split(line, ",")
+		x, _ := strconv.Atoi(strings.TrimSpace(coords[0]))
+		y, _ := strconv.Atoi(strings.TrimSpace(coords[1]))
+
+		if x < xMin {
+			xMin = x
+		}
+		if x > xMax {
+			xMax = x
+		}
+		if y > yMax {
+			yMax = y
+		}
+		if y < yMin {
+			yMin = y
+		}
+	}
+
+	grid := make([][]*Point, yMax-yMin+1)
+	for i := range grid {
+		grid[i] = make([]*Point, xMax-xMin+1)
+	}
 
 	for i, line := range lines {
 		coords := strings.Split(line, ",")
 		x, _ := strconv.Atoi(strings.TrimSpace(coords[0]))
 		y, _ := strconv.Atoi(strings.TrimSpace(coords[1]))
 
-		locations = append(locations, Location{name: strconv.Itoa(i), point: Point{x: x, y: y}})
+		grid[y-yMin][x-xMin] = &Point{closestLocation: i, isBaseLocation: true}
 	}
 
-	var largestX, largestY int = locations[0].point.x, locations[0].point.y
-	var smallestX, smallestY int = locations[0].point.x, locations[0].point.y
-	for _, l := range locations {
-		if l.point.x > largestX {
-			largestX = l.point.x
-		}
-		if l.point.y > largestY {
-			largestY = l.point.y
-		}
-		if l.point.x < smallestX {
-			smallestX = l.point.x
-		}
-		if l.point.y < smallestY {
-			smallestY = l.point.y
-		}
+	return grid, len(lines)
+}
+
+func (p *Point) FindClosestLocation(x, y int, grid [][]*Point) {
+	if p == nil {
+		p = &Point{}
+		grid[y][x] = p
 	}
 
-	for i, l := range locations {
-		if l.point.x == largestX || l.point.x == smallestX || l.point.y == largestY || l.point.y == smallestY {
-			locations[i].infinte = true
-		} else {
-			locations[i].infinte = false
-		}
+	p.FindTotalDistance(x, y, grid)
+
+	if p.isBaseLocation {
+		return
 	}
 
-	var allPoints []Point
+	minDistance, multi := math.MaxInt32, false
+	var closestLocation *Point
 
-	for x := smallestX; x <= largestX; x++ {
-		for y := smallestY; y <= largestY; y++ {
-			allPoints = append(allPoints, Point{x: x, y: y})
-		}
-	}
-
-	for i, p := range allPoints {
-		closestDistance := math.MaxInt32
-		for _, l := range locations {
-			if l.point.x == p.x && l.point.y == p.y {
-				allPoints[i].closestLocation = "location"
-				break
-			}
-
-			distance := int(math.Abs(float64(l.point.x-p.x)) + math.Abs(float64(l.point.y-p.y)))
-
-			if distance < closestDistance {
-				closestDistance = distance
-			}
-		}
-
-		if allPoints[i].closestLocation != "location" {
-			for _, l := range locations {
-				distance := int(math.Abs(float64(l.point.x-p.x)) + math.Abs(float64(l.point.y-p.y)))
-
-				if distance == closestDistance {
-					if allPoints[i].closestLocation != "" {
-						allPoints[i].closestLocation = "multi"
-					} else {
-						allPoints[i].closestLocation = l.name
+	for yTest, line := range grid {
+		for xTest, pTest := range line {
+			if pTest != nil {
+				if pTest.isBaseLocation {
+					distance := Abs(xTest-x) + Abs(yTest-y)
+					if distance < minDistance {
+						minDistance = distance
+						closestLocation = pTest
+						multi = false
+					} else if distance == minDistance {
+						multi = true
 					}
 				}
 			}
 		}
 	}
 
-	for _, p := range allPoints {
-		if p.closestLocation == "multi" {
-			continue
-		}
+	if multi {
+		p.closestLocation = -1
+	} else {
+		p.closestLocation = closestLocation.closestLocation
+	}
 
-		for i, l := range locations {
-			if l.name == p.closestLocation {
-				locations[i].area++
-				break
+}
+
+func (p *Point) FindTotalDistance(x, y int, grid [][]*Point) {
+	if p == nil {
+		p = &Point{}
+		grid[y][x] = p
+	}
+
+	for yTest, line := range grid {
+		for xTest, pTest := range line {
+			if pTest != nil {
+				if pTest.isBaseLocation {
+					p.distanceToAllLocations += Abs(x-xTest) + Abs(y-yTest)
+				}
+			}
+		}
+	}
+}
+
+func GetArea(location int, grid [][]*Point) (area int, isInfinite bool) {
+
+	for y, line := range grid {
+		for x, p := range line {
+			if p.closestLocation == location {
+				area++
+
+				if x == 0 || y == 0 || x == len(line)-1 || y == len(grid)-1 {
+					return area, true
+				}
 			}
 		}
 	}
 
-	var largestArea int
-	for _, l := range locations {
-		if !l.infinte {
-			if l.area > largestArea {
-				largestArea = l.area
-			}
-		}
+	return area, false
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
 	}
-
-	fmt.Printf("Part A: %v\n", largestArea+1)
-
-}
-
-type Location struct {
-	name    string
-	point   Point
-	infinte bool
-	area    int
-}
-
-type Point struct {
-	x, y            int
-	closestLocation string
+	return x
 }
